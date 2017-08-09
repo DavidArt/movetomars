c0bcccf836a1ea721c328a60d35404af4a3833c2
Rus David <davidrus.art@gmail.com>
2017-08-09 08:45:05 +0300

    Initial commit New Gradle Project

---
+++ src/main/java/com/davidrus/movetomars/MovetomarsApplication.java
@@ -0,0 +1,12 @@
+package com.davidrus.movetomars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovetomarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovetomarsApplication.class, args);
+	}
+}
