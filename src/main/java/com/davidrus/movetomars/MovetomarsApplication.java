fe1af7c70f854aedab7571cc5e609a2a13d1228f
Rus David <davidrus.art@gmail.com>
2017-08-09 09:24:09 +0300

    Added ApiConfig class for deserialization of Jsons into Pojos and serialization of Pojos into Jsons
    Annotated the MovetomarsApplication
    
    more .gitignore config

--- src/main/java/com/davidrus/movetomars/MovetomarsApplication.java
+++ src/main/java/com/davidrus/movetomars/MovetomarsApplication.java
@@ -1,9 +1,20 @@
 package com.davidrus.movetomars;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Main point for starting the application
+ *
+ * Automated bean configuration
+ * Automated scan for configuration classes to wire up
+ * in the Spring Application Context
+ */
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class MovetomarsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/davidrus/movetomars/config/ApiConfig.java
@@ -0,0 +1,38 @@
+package com.davidrus.movetomars.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for deserialization of Jsona into
+ * Pojos, and serialization of Pojos into Jsons
+ */
+@Configuration
+public class ApiConfig {
+
+    /**
+     * ObjectMapper defines how Json strings from the request body
+     * are deserialized in POJO (Plain Old Java Object).
+     */
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    /**
+     * ObjectWriter defines how we serialize the POJO into
+     * a Json string in the response body.
+     *
+     * @param objectMapper the param to take
+     * @return a ObjectWriter object
+     *
+     * We use the ObjectMapper to create a default PrettyPrinter
+     * which will output a Json in a human readable format
+     */
+    @Bean
+    public ObjectWriter objectWriter(ObjectMapper objectMapper) {
+        return objectMapper.writerWithDefaultPrettyPrinter();
+    }
+}
