4ce43f2fd76819b1db6788df2e00ec7218986fca
Rus David <davidrus.art@gmail.com>
2017-08-15 20:33:59 +0300

    - Updated app.component.html, added a checkin and checkout form - Imported and implemented ReactiveFormModule used for creating a form to allow users to search for available modules - Imported and implemented Twitter Bootstrap CSS

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup} from "@angular/forms"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'Visit Mars like you have never done it before';
+
+  /**
+   * variable used to check if our form was submited
+   */
+  public submitted:boolean;
+
+  /**
+   * variable used for our FormGroup
+   */
+  roomsearch : FormGroup;
+
+  /**
+   * Method used to initialize our form group
+   */
+  ngOnInit() {
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  /**
+   * Method used to trigger the action when our form is submitted
+   * Indicates if our form was submitted
+   */
+  onSubmit({value,valid}: {value:Roomsearch, valid:boolean}) {
+    console.log(value);
+  }
+
+}
+
+export interface Roomsearch {
+  checkin:string;
+  checkout:string;
 }
