8e6b46e51335905d762f63839e2852d5788a79e4
Rus David <davidrus.art@gmail.com>
2017-08-15 23:49:26 +0300

    - Created a dynamically populated table to display search results - Created hardcoded Jsons for testing purpose only - used the ngFor directive to populate data into table - used the Twitter bootstrap classes

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,46 +1,91 @@
 import {Component, OnInit} from '@angular/core';
-import { FormControl, FormGroup} from "@angular/forms"
+import {FormControl, FormGroup} from "@angular/forms"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   title = 'Visit Mars like you have never done it before';
 
   /**
    * variable used to check if our form was submited
    */
-  public submitted:boolean;
+  public submitted: boolean;
 
   /**
    * variable used for our FormGroup
    */
-  roomsearch : FormGroup;
+  modulesearch: FormGroup;
+
+  /**
+   * an array of modules
+   */
+  modules: Module[];
 
   /**
    * Method used to initialize our form group
    */
   ngOnInit() {
-    this.roomsearch = new FormGroup({
+    this.modulesearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+
+    this.modules = MODULES;
   }
 
   /**
    * Method used to trigger the action when our form is submitted
    * Indicates if our form was submitted
    */
-  onSubmit({value,valid}: {value:Roomsearch, valid:boolean}) {
+  onSubmit({value, valid}: { value: Modulesearch, valid: boolean }) {
     console.log(value);
   }
 
 }
 
-export interface Roomsearch {
-  checkin:string;
-  checkout:string;
+/**
+ * Interface used for searching a module
+ */
+export interface Modulesearch {
+  checkin: string;
+  checkout: string;
 }
+
+/**
+ * Interface representing a contract for a Module component
+ */
+export interface Module {
+  id: string;
+  moduleNumber: string;
+  price: string;
+  links: string;
+}
+
+/**
+ * hardcoded Json for testing
+ * @type {[{}]}
+ */
+var MODULES:Module[] = [
+  {
+    "id" : "325656",
+    "moduleNumber" : "587",
+    "price" : "200",
+    "links" : ""
+  },
+  {
+    "id" : "5766772",
+    "moduleNumber" : "588",
+    "price" : "250",
+    "links" : ""
+  },
+  {
+    "id" : "2525337",
+    "moduleNumber" : "589",
+    "price" : "300",
+    "links" : ""
+  }
+];
