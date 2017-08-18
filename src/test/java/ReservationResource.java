fd3a294f66f743d608f1440e5ea0b14c934436c4
Rus David <davidrus.art@gmail.com>
2017-08-18 16:34:30 +0300

    Unit Testing

---
+++ src/test/java/ReservationResource.java
@@ -0,0 +1,36 @@
+import com.davidrus.movetomars.MovetomarsApplication;
+import com.davidrus.movetomars.rest.ResourceConstants;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MovetomarsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ReservationResource {
+
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setUp() throws Exception {
+
+        RestAssured.port = Integer.valueOf(port);
+        RestAssured.basePath = ResourceConstants.MODULE_RESERVATION_V1;
+        RestAssured.baseURI = "http://localhost";
+    }
+
+    @Test
+    public void test() {
+
+        given().when().get("/" + 1).then().statusCode(200);
+    }
+
+}
